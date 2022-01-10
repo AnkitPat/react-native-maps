ef6513f3da979dc7212d2f2cdc5bdbef46ecf1d4
AnkitPat <ankitpatidar030@gmail.com>
2022-01-10 19:52:22 +0530

    Update AirMapLocalTile.java

--- lib/android/src/main/java/com/airbnb/android/react/maps/AirMapLocalTile.java
+++ lib/android/src/main/java/com/airbnb/android/react/maps/AirMapLocalTile.java
@@ -1,6 +1,11 @@
 package com.airbnb.android.react.maps;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Tile;
@@ -13,6 +18,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class AirMapLocalTile extends AirMapFeature {
 
@@ -20,17 +27,136 @@ public class AirMapLocalTile extends AirMapFeature {
         private static final int BUFFER_SIZE = 16 * 1024;
         private int tileSize;
         private String pathTemplate;
+        private String path;
+        private String flavourName;
 
 
         public AIRMapLocalTileProvider(int tileSizet, String pathTemplate) {
             this.tileSize = tileSizet;
             this.pathTemplate = pathTemplate;
+            Log.v("Check>>>C", "Created");
+            for (File file :
+                    getContext().getExternalFilesDirs(null)) {
+                Log.v("Check>>>>", file.getAbsolutePath());
+            }
+
+            SharedPreferences sh = getContext().getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
+
+// The value will be default as empty string because for
+// the very first time when the app is opened, there is nothing to show
+            String storageType = sh.getString("storageType", "internal");
+            File[] files = getContext().getExternalFilesDirs(null);
+            if (storageType.equals("external")) {
+
+
+                if (files.length > 1)
+                    this.path = files[1].getAbsolutePath();
+                else
+                    this.path = getContext().getFilesDir().getAbsolutePath();
+
+            } else {
+                this.path = getContext().getFilesDir().getAbsolutePath();
+            }
+
+            this.flavourName = getBuildConfigValue("VARIANT").toString().toUpperCase();
+        }
+
+        Object getBuildConfigValue(String fieldName) {
+            try {
+                Class c = Class.forName("eu.acsi.BuildConfig");
+                Field f = c.getDeclaredField(fieldName);
+                f.setAccessible(true);
+                return f.get(null);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
         }
 
         @Override
         public Tile getTile(int x, int y, int zoom) {
-            byte[] image = readTileImage(x, y, zoom);
-            return image == null ? TileProvider.NO_TILE : new Tile(this.tileSize, this.tileSize, image);
+            // TODO
+
+            String path = this.path + "/data/" + this.flavourName + "/";
+            Log.v("Check>>>", path);
+            SQLiteDatabase database = SQLiteDatabase.openDatabase(path + "maptilesindex.sqlite", null, 0);
+            String tileQuery = "SELECT PackageID FROM ZoomLevelTileRange WHERE ZoomLevel=? AND (? BETWEEN StartX And EndX) AND  (? BETWEEN StartY And EndY)";
+            Cursor tileCursor = database.rawQuery(tileQuery, new String[]{zoom + "", x + "", y + ""});
+            try {
+
+                if (tileCursor.moveToFirst()) {
+                    ArrayList<Integer> tileRows = new ArrayList<>();
+
+                    do {
+                        int tileRow = tileCursor.getInt(tileCursor.getColumnIndex("PackageID"));
+                        Log.v("Check>>>", tileRow + "," + x + "," + y + "," + zoom + "," + tileCursor);
+                        tileRows.add(tileRow);
+
+                    } while (tileCursor.moveToNext());
+                    tileCursor.close();
+                    database.close();
+                    for (Integer row :
+                            tileRows) {
+                        if (row == 0) {
+                            SQLiteDatabase databaseMapTile = SQLiteDatabase.openDatabase(path + "maptiles.sqlite", null, 0);
+                            String tileQueryMapTile = "SELECT ImageData FROM Tile WHERE ZoomLevel=? AND X=? and Y=?";
+                            Cursor tileCursorMapTile = databaseMapTile.rawQuery(tileQueryMapTile, new String[]{zoom + "", x + "", y + ""});
+                            try {
+                                if (tileCursorMapTile.moveToFirst()) {
+                                    if (!tileCursorMapTile.isAfterLast()) {
+                                        byte[] tileData = tileCursorMapTile.getBlob(tileCursorMapTile.getColumnIndex("ImageData"));
+                                        if (tileData != null) {
+                                            Log.v("Check>>>", tileData.toString());
+                                            byte[] image = tileData;
+                                            return image == null ? TileProvider.NO_TILE : new Tile(this.tileSize, this.tileSize, image);
+
+                                        }
+
+                                    }
+
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                tileCursorMapTile.close();
+                                databaseMapTile.close();
+                            }
+                        } else if (new File(path + "maptiles" + row + ".sqlite").exists()) {
+                            SQLiteDatabase databaseMapTile = SQLiteDatabase.openDatabase(path + "maptiles" + row + ".sqlite", null, 0);
+                            String tileQueryMapTile = "SELECT ImageData FROM Tile WHERE ZoomLevel=? AND X=? and Y=?";
+                            Cursor tileCursorMapTile = databaseMapTile.rawQuery(tileQueryMapTile, new String[]{zoom + "", x + "", y + ""});
+                            try {
+                                if (tileCursorMapTile.moveToFirst()) {
+                                    if (!tileCursorMapTile.isAfterLast()) {
+                                        byte[] tileData = tileCursorMapTile.getBlob(tileCursorMapTile.getColumnIndex("ImageData"));
+                                        if (tileData != null) {
+                                            Log.v("Check>>>", tileData.toString());
+                                            byte[] image = tileData;
+                                            return image == null ? new Tile(this.tileSize, this.tileSize, readTileImage(this.path + "/data/no_maptiles1.png")) : new Tile(this.tileSize, this.tileSize, image);
+
+                                        }
+
+                                    }
+
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            } finally {
+                                tileCursorMapTile.close();
+                                databaseMapTile.close();
+                            }
+                        }
+                    }
+
+                }
+            } catch (Exception e) {
+                Log.v("Check>>>", e.getMessage());
+            } finally {
+                tileCursor.close();
+                database.close();
+            }
+
+            return new Tile(this.tileSize, this.tileSize, readTileImage(this.path + "/data/no_maptiles1.png"));
         }
 
         public void setPathTemplate(String pathTemplate) {
@@ -41,10 +167,10 @@ public class AirMapLocalTile extends AirMapFeature {
             this.tileSize = tileSize;
         }
 
-        private byte[] readTileImage(int x, int y, int zoom) {
+        private byte[] readTileImage(String path) {
             InputStream in = null;
             ByteArrayOutputStream buffer = null;
-            File file = new File(getTileFilename(x, y, zoom));
+            File file = new File(path);
 
             try {
                 in = new FileInputStream(file);
@@ -66,8 +192,14 @@ public class AirMapLocalTile extends AirMapFeature {
                 e.printStackTrace();
                 return null;
             } finally {
-                if (in != null) try { in.close(); } catch (Exception ignored) {}
-                if (buffer != null) try { buffer.close(); } catch (Exception ignored) {}
+                if (in != null) try {
+                    in.close();
+                } catch (Exception ignored) {
+                }
+                if (buffer != null) try {
+                    buffer.close();
+                } catch (Exception ignored) {
+                }
             }
         }
 
@@ -112,7 +244,7 @@ public class AirMapLocalTile extends AirMapFeature {
     public void setTileSize(float tileSize) {
         this.tileSize = tileSize;
         if (tileProvider != null) {
-            tileProvider.setTileSize((int)tileSize);
+            tileProvider.setTileSize((int) tileSize);
         }
     }
 
@@ -126,7 +258,7 @@ public class AirMapLocalTile extends AirMapFeature {
     private TileOverlayOptions createTileOverlayOptions() {
         TileOverlayOptions options = new TileOverlayOptions();
         options.zIndex(zIndex);
-        this.tileProvider = new AirMapLocalTile.AIRMapLocalTileProvider((int)this.tileSize, this.pathTemplate);
+        this.tileProvider = new AirMapLocalTile.AIRMapLocalTileProvider((int) this.tileSize, this.pathTemplate);
         options.tileProvider(this.tileProvider);
         return options;
     }
